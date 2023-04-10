3d0066ffffe4e29f60ee0ccf950b2d6341556d2e
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-04-11 00:32:52 +0200

    fix dark mode and responsive design

--- components/product/DetailProduct.vue
+++ components/product/DetailProduct.vue
@@ -2,9 +2,9 @@
     <section>
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-                <h2 class="text-2xl font-bold text-gray-900 text-center">L'intérieur du futur</h2>
+                <h2 class="text-2xl font-bold text-gray-900 dark:text-gray-200 text-center">L'intérieur du futur</h2>
 
-                <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0 flex flex-col">
+                <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0 dark:text-white">
                     <div v-for="callout in callouts" :key="callout.name" class="group relative">
                         <div
                             class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
@@ -12,13 +12,13 @@
                                 class="h-full w-full object-cover object-center"></nuxt-img>
                         </div>
                         <div class="">
-                            <h3 class="mt-6 text-sm text-gray-500">
+                            <h3 class="mt-6 text-sm text-gray-500 dark:text-gray-300">
                                 <a :href="callout.href">
                                     <span class="absolute inset-0" />
                                     {{ callout.name }}
                                 </a>
                             </h3>
-                            <p class="text-base font-semibold text-gray-900">{{ callout.description }}</p>
+                            <p class="text-base font-semibold text-gray-900 dark:text-white">{{ callout.description }}</p>
                         </div>
                     </div>
                 </div>
--- components/product/VideoProduct.vue
+++ components/product/VideoProduct.vue
@@ -1,13 +1,13 @@
 <template>
-    <section class="flex flex-row-reverse">
-        <div class="flex flex-col flex-wrap ml-5 mt-10 w-4/12">
+    <section class="flex flex-row-reverse sm:flew-col dark:text-white">
+        <div class="flex flex-col flex-wrap ml-5 mt-10 w-4/12 video-content">
             <span class="text-lg">Autonomie</span>
             <span class="text-xl subpixel-antialiased mb-8 font-bold">Voyager sans limite</span>
             <p class="text-sm">Avec une autonomie estimée pouvant atteindre 634 kilomètres et un accès au réseau de recharge
                 rapide le plus vaste et le plus puissant au monde, vous passerez moins de temps à recharger et plus de temps
                 sur la route.</p>
         </div>
-        <div class="w-8/12">
+        <div class="w-8/12 video">
             <video controls>
                 <source src="/video/video-car.mp4" type="video/mp4">
                 Votre navigateur ne supporte pas la lecture de cette vidéo.
@@ -15,3 +15,22 @@
         </div>
     </section>
 </template>
+
+<style scoped>
+
+@media (max-width:1024px) {
+    section {
+        flex-direction: column-reverse;
+    }
+    .video{
+        width: 100%;
+    }
+    
+    .video-content {
+        margin: auto;
+        width: auto;
+        justify-content: center;
+        align-items: center;
+    }
+}
+</style>
