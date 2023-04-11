1d76f02e184b477c3827489a7249fd5811e50363
jehanebnj <jehanebnj@gmail.com>
2023-04-11 17:58:09 +0200

    add navbar component

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,6 @@
 <template>
     <header>
-        <nav>
-            <router-link to="/">Green CAR</router-link>
-            <ul class="menu">
-                <li><router-link to="/">Green CAR</router-link></li>
-                <li>
-                    <router-link to="/product">Product</router-link>
-                </li>
-                <li><router-link to="/">Contact</router-link></li>
-            </ul>
-
-            <div class="menu-btn">
-                <img src="@/assets/img/burger.svg" alt="Icône burger pour barre de navigation">
-            </div>
-        </nav>
-
+        <Navbar/>
         <div class="header-body">
             <div>
                 <h1 class="text-3xl">Test</h1>
@@ -31,35 +17,6 @@
 
 
 <style scoped>
-a {
-    text-decoration: none;
-    color: #fff;
-}
-
-
-nav,
-li {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-nav {
-    padding: 1em 5em;
-    position: relative;
-    border-bottom: 1px solid #eee;
-}
-
-nav li {
-    display: inline-block;
-    margin: 0 1.7em;
-    text-align: center;
-}
-
-nav a:hover {
-    color: #eee;
-
-}
 
 header {
     height: 100vh;
---
+++ components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <nav>
+            <router-link to="/">Green CAR</router-link>
+            <ul class="menu">
+                <li><router-link to="/">Green CAR</router-link></li>
+                <li>
+                    <router-link to="/product">Product</router-link>
+                </li>
+                <li><router-link to="/">Contact</router-link></li>
+            </ul>
+
+            <div class="menu-btn">
+                <img src="@/assets/img/burger.svg" alt="Icône burger pour barre de navigation">
+            </div>
+        </nav>
+</template>
+
+<style scoped>
+a {
+    text-decoration: none;
+    color: #fff;
+}
+
+nav,
+li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+nav {
+    padding: 1em 5em;
+    position: relative;
+    border-bottom: 1px solid #eee;
+}
+
+nav li {
+    display: inline-block;
+    margin: 0 1.7em;
+    text-align: center;
+}
+
+nav a:hover {
+    color: #eee;
+
+}
+.menu-btn {
+    display: none;
+}
+
+</style>
