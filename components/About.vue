236d50e4d48080de7b3dd72bbc564926583ed737
waseem1104 <waseem11@hotmail.fr>
2023-04-11 17:52:32 +0200

    Add dark class in About + add contains

--- components/About.vue
+++ components/About.vue
@@ -1,16 +1,30 @@
 <template>
     <section class="about">
 
-        <div class="heading">
-            <h2>Green CAR,<br/>100 éléctrique</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum distinctio quod accusamus dolorem vero quaerat sequi beatae provident ex commodi maiores accusantium eius mollitia, omnis nam nisi asperiores, quis blanditiis?</p>
-        </div>
-        
-        <div class="img-container">
-            <nuxt-img format="avif" lazy="loading" class="img-container" src="/header.jpg" alt="une voiture rouge en pleine vitesse"></nuxt-img>
-        </div>
+        <section class="bg-white dark:bg-gray-950">
+            <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+               <nuxt-img format="avif" lazy="loading" class="img-container rounded" src="/header.jpg" alt="une voiture rouge en pleine vitesse"></nuxt-img>
+                <div class="mt-4 md:mt-0">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Green CAR,<br/>100 éléctrique</h2>
+                    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
+                </div>
+            </div>
+
+
+             <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+                <div class="mt-4 md:mt-0">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Green CAR,<br/>100 éléctrique</h2>
+                    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
+                </div>
+
+                 <nuxt-img format="avif" lazy="loading" class="rounded" src="/header.jpg" alt="une voiture rouge en pleine vitesse"></nuxt-img>
+            </div>
+        </section>
 
     </section>
+
+
+    
 </template>
 
 
@@ -21,28 +35,4 @@
         border-left: 0.1em #0074e8 solid;
     }
 
-    .heading p {
-        margin-left: 3em;
-        
-    }
-
-    .about{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        gap: 2em;
-    }
-
-    .img-container{
-        flex: 1 1 35rem;
-    }
-
-    .img-container img{
-       width: 100%;
-    }
-
-    .heading{
-        flex: 1 1 35rem;
-        padding-left: 2em;
-    }
 </style>
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
    
-    <footer class="bg-white dark:bg-gray-900 m-4">
+    <footer class="bg-white dark:bg-gray-900">
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div class="sm:flex sm:items-center sm:justify-between">
                 <span>
