5f422bae9d3b5cc466892af7baea076d664cc297
jehanebnj <jehanebnj@gmail.com>
2023-04-13 11:40:27 +0200

    add FAQ page

---
+++ pages/contact.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <Navbar />
+        <Faq />
+    </div>
+</template>
+
+<script setup>
+import Faq from '~~/components/contact/Faq.vue';
+import Navbar from '~~/components/Navbar.vue';
+</script>
