246ea30acc70a96112be95ca05069b732f760190
waseem1104 <waseem11@hotmail.fr>
2023-04-14 01:10:14 +0200

    add muted in video

--- components/product/VideoProduct.vue
+++ components/product/VideoProduct.vue
@@ -5,7 +5,7 @@
         <section class="bg-gray-100 dark:bg-gray-950">
             <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
               <div class="video">
-                    <video class="rounded-lg" controls>
+                    <video class="rounded-lg" controls muted>
                         <source src="/video/video-car.mp4" type="video/mp4">
                         Votre navigateur ne supporte pas la lecture de cette vidéo.
                     </video>
