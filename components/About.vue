853b5ef827b74b7141c9ce84736208ce86851eef
jehanebnj <jehanebnj@gmail.com>
2023-04-14 00:27:44 +0200

    fix lazy loading

--- components/About.vue
+++ components/About.vue
@@ -3,7 +3,7 @@
 
         <section class="bg-white dark:bg-gray-950">
             <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-               <nuxt-img format="avif" lazy="loading" class="img-container rounded" src="/header.jpg" alt="une voiture rouge en pleine vitesse"></nuxt-img>
+               <nuxt-img format="avif" loading=”lazy” class="img-container rounded" src="/header.jpg" alt="une voiture rouge en pleine vitesse"></nuxt-img>
                 <div class="mt-4 md:mt-0">
                     <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Green CAR,<br/>{{$t("i6")}}</h2>
                     <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">{{ $t("i7") }}</p>
@@ -17,7 +17,7 @@
                     <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">{{ $t("i9") }}</p>
                 </div>
 
-                 <nuxt-img format="avif" lazy="loading" class="rounded" src="/header.jpg" alt="une voiture rouge en pleine vitesse"></nuxt-img>
+                 <nuxt-img format="avif" loading=”lazy” class="rounded" src="/header.jpg" alt="une voiture rouge en pleine vitesse"></nuxt-img>
             </div>
         </section>
 
