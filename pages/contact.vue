01aaff914791e0e549e82c7678f3a98f0e303c80
jehanebnj <jehanebnj@gmail.com>
2023-04-13 11:46:58 +0200

    add contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,10 +2,12 @@
     <div>
         <Navbar />
         <Faq />
+        <Form />
     </div>
 </template>
 
 <script setup>
 import Faq from '~~/components/contact/Faq.vue';
 import Navbar from '~~/components/Navbar.vue';
+import Form from '~~/components/contact/Form.vue';
 </script>
